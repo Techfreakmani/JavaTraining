e60ed0b850cb3099be568ec5e080eae4365ea8d7
Techfreakmani <manimani67784@gmail.com>
2025-02-11 11:05:11 +0530

    Day 5

--- src/com/celcom/day5/InterfaceExample4.java
+++ src/com/celcom/day5/InterfaceExample4.java
@@ -1,10 +1,38 @@
 package com.celcom.day5;
 
+
+interface InterfaceOne{
+	
+	int a = 10;
+	//by default interface variables are public static and final
+	//by default interface methods are public abstract
+	
+}
+
+interface InterfaceTwo{
+	
+	int a =20;
+}
+
+interface InterfaceThree extends InterfaceOne,InterfaceTwo{
+	
+	void addition();
+	
+}
+
+class Addition implements InterfaceThree{
+	
+	public void addition()
+	{
+		System.out.println(InterfaceOne.a + InterfaceTwo.a);
+	}
+}
 public class InterfaceExample4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Addition add = new Addition();
+		add.addition();
 	}
 
 }
