9a5cea1ad6ae488ed154e6f609a0dbc9c0b058f0
Techfreakmani <manimani67784@gmail.com>
2025-02-12 09:32:34 -0800

    Day6

--- src/com/celcom/day6/Assignment2.java
+++ src/com/celcom/day6/Assignment2.java
@@ -2,12 +2,18 @@ package com.celcom.day6;
 
 import java.util.Scanner;
 
-
+class VowelFound extends Exception{
+	
+	public String toString() {
+		return "Vowel Found";
+	}
+}
 
 public class Assignment2 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter any Name");
 		String name = sc.next();
 		
 		isNotVowel(name);
@@ -20,8 +26,17 @@ public class Assignment2 {
 			int count = 0;
 			for(int i = 0; i < name.length(); i++)
 			{
-				if(name.charAt(i) == 'a'|| name.charAt(i) == 'e' || name.charAt(i) == 'i' || name.charAt(i) == 'o' || name.charAt(i) =='u') ||)
+				if(name.charAt(i) == 'a'|| name.charAt(i) == 'e' || name.charAt(i) == 'i' || name.charAt(i) == 'o' || name.charAt(i) =='u' || name.charAt(i) =='A' || name.charAt(i) == 'E' || name.charAt(i) =='I' || name.charAt(i) =='O' || name.charAt(i) =='U'){
+					count ++;
+				}
 			}
+			if(count > 0)
+			{
+				throw new VowelFound();
+			}
+		}catch(VowelFound e)
+		{
+			System.out.println(e);
 		}
 		
 		
